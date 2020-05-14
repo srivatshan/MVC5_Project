1ca889072cb3360f6350e99631d19aaca79bf798
srivatshan br <srirenga96@gmail.com>
2020-05-14 13:08:42 +0000

    Home Controller, Authendication Implementation

---
+++ MVCProject/MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVCProject.Data.Models;
+using MVCProject.Filters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    [AuthendicationFilter]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Getting User Details from Cookies or session
+            User UserDetails = (User)Session["UserDetails"] ?? JsonConvert.DeserializeObject<User>(Request.Cookies["UserDetails"].Value);
+            if (UserDetails.UserType == "Admin")
+            {
+                ViewBag.SearchEnable = true;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MVCProject/MVCProject/Filters/AuthendicationFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+
+namespace MVCProject.Filters
+{
+    public class AuthendicationFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            var session = filterContext.HttpContext.Session["UserDetails"] ?? filterContext.HttpContext.Request.Cookies.Get("UserDetails");
+            if ((string.IsNullOrEmpty(Convert.ToString(session))))
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                 new RouteValueDictionary
+                 {
+                     { "controller", "Account" },
+                     { "action", "Login" }
+                 });
+            }
+        }
+    }
+}
